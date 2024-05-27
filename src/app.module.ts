5b0366791dede06f26f8bf6778500ebd1e0f529a
Cristian <do.cristian.tr@hotmail.com>
2024-05-27 00:35:58 -0400

    feat: docker compose & env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: ['.env.development', '.env.production']
+    })
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 
 import { AppModule } from './app.module';
 import { appConfig, swaggerOptions, validationOptions } from './config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, appConfig);
   const document = SwaggerModule.createDocument(app, swaggerOptions);
+  const configService: ConfigService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe(validationOptions))
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT'));
+  console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
