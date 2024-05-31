c107dbfd0c376b3efaecc83d328783d967d2741c
Cristian <do.cristian.tr@hotmail.com>
2024-05-31 10:59:42 -0400

    feat: product repository

--- src/modules/products/domain/mocks/product.mock.ts
+++ src/modules/products/domain/mocks/product.mock.ts
@@ -1,15 +1,14 @@
-// import { Product } from "@prisma/client";
-// import {}
+import { Product } from "@prisma/client";
 
-// export const products: Product[] = [];
+export const products: Product[] = [];
 
-// export const product: Product = {
-//     id: '550e8400-e29b-41d4-a716-446655440000',
-//     name: 'Cloro',
-//     amount: 5,
-//     price: 105,
-//     userId: 
-//     unit_measure: '1 galon',
-//     category_name: 'Limpieza',
-//     expiration: new Date(),
-// };
+export const product: Product = {
+    id: '550e8400-e29b-41d4-a716-446655440000',
+    name: 'Cloro',
+    amount: 5,
+    price: 135,
+    userId: '',
+    unit_measure: '1 galon',
+    category_name: 'Limpieza',
+    expiration: new Date(),
+};
