59eac0fdc477db61fe89b5540f23b1d62b4b5d00
Cristian <do.cristian.tr@hotmail.com>
2024-06-06 17:46:33 -0400

    test: fix customer repository tests

--- src/modules/customers/application/repositories/customer.repository.spec.ts
+++ src/modules/customers/application/repositories/customer.repository.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerRepository } from './customer.repository';
 import { PrismaProvider } from '../../../prisma/providers/prisma.provider';
 import { prismaMock } from '../../domain/mocks/customers-providers.mock';
-import { mockCustomer1, mockCustomer2, mockCustomers } from '../../domain/mocks/customers.mock';
+import { mockCustomer1, mockCustomer2, mockCustomer3, mockCustomers } from '../../domain/mocks/customers.mock';
 import { CustomerEntity } from '../../domain/entities/customer.entity';
 
 describe('Customer', () => {
@@ -31,42 +31,76 @@ describe('Customer', () => {
 
   describe('Find All Customers', () => {
     it('should find all customers', async () => {
-      jest.spyOn(prisma.customer, 'findMany').mockRejectedValue([mockCustomers[0], mockCustomers[1]]);
+      jest.spyOn(prisma.customer, 'findMany').mockResolvedValue([ mockCustomer1, mockCustomer3 ]);
 
       const res: Partial<CustomerEntity>[] = await repository.findAll(mockCustomers[0].user_id, { skip: 0, take: 3});
 
       expect(res).toBeInstanceOf(Array);
       expect(res).toHaveLength(2);
-      expect(res).toBe([ mockCustomer1, mockCustomer2 ]);
+      expect(res).toEqual([ mockCustomer1, mockCustomer3 ]);
     });
 
     it('should only bring one element', async () => {
-      jest.spyOn(prisma.customer, 'findMany').mockRejectedValue([mockCustomers[0]]);
+      jest.spyOn(prisma.customer, 'findMany').mockResolvedValue([mockCustomers[0]]);
 
       const res: Partial<CustomerEntity>[] = await repository.findAll(mockCustomers[0].user_id, { skip: 0, take: 1 });
 
       expect(res).toBeInstanceOf(Array);
       expect(res).toHaveLength(1);
-      expect(res).toBe([ mockCustomer1 ]);
+      expect(res).toEqual([ mockCustomer1 ]);
     });
   });
 
   describe('Find One Customer', () => {
     it('should find a customer', async () => {
-      jest.spyOn(prisma.customer, 'findUnique').mockRejectedValue(mockCustomers[0]);
+      jest.spyOn(prisma.customer, 'findUnique').mockResolvedValue(mockCustomers[0]);
+
+      const res: Partial<CustomerEntity> = await repository.findOne(mockCustomer1.id);
+
+      expect(res).toEqual(mockCustomer1);
+    });
+
+    it('should fetch some fields from a customer', async () => {
+      jest.spyOn(prisma.customer, 'findUnique').mockResolvedValue(mockCustomers[0]);
 
       const res: Partial<CustomerEntity> = await repository.findOne(mockCustomer1.id, {
         name: true,
         companies: false
       });
 
-      expect(res).toBe(mockCustomer1);
+      expect(res).toEqual(mockCustomer1);
     });
   });
 
-  describe('Create Customer', () => {});
+  describe('Create Customer', () => {
+    it('should create a customer', async () => {
+      jest.spyOn(prisma.customer, 'create').mockResolvedValue(mockCustomers[0]);
 
-  describe('Update Customer', () => {});
+      const { name, contact, companies, user_id } = mockCustomers[0];
+      const res: Partial<CustomerEntity> = await repository.create({ name, contact, companies, user_id });
 
-  describe('Delete Customer', () => {});
+      expect(res).toEqual(mockCustomer1);
+    });
+  });
+
+  describe('Update Customer', () => {
+    it('should update a customer', async () => {
+      jest.spyOn(prisma.customer, 'update').mockResolvedValue(mockCustomer3);
+
+      const { name, contact, companies } = mockCustomer2;
+      const res: Partial<CustomerEntity> = await repository.update(mockCustomer1.id, { name, contact, companies });
+
+      expect(res).toEqual(mockCustomer3);
+    });
+  });
+
+  describe('Delete Customer', () => {
+    it('should delete a customer', async () => {
+      jest.spyOn(prisma.customer, 'delete').mockResolvedValue(mockCustomer3);
+
+      const res: Partial<CustomerEntity> = await repository.delete(mockCustomer3.id);
+
+      expect(res).toEqual(mockCustomer3);
+    });
+  });
 });
