3e854441d0cd29ef2d72d3708a6ec16dd1b77d1b
Cristian <do.cristian.tr@hotmail.com>
2024-06-07 17:03:57 -0400

    feat: inventory's dto

---
+++ src/modules/inventory/infrastructure/dtos/create-inventory.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
+
+export class CreateInventoryDto {
+    @ApiProperty({ example: 'Ferreteria', description: 'Financial capital' })
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(200)
+    company_name: string;
+
+    @ApiProperty({ example: 30000.00, description: 'Financial capital' })
+    @IsNotEmpty()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    capital: number;
+
+    @ApiProperty({ example: 5000.0, description: 'The amount they have to pay you' })
+    @IsNotEmpty()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    service_charge: number;
+}
---
+++ src/modules/inventory/infrastructure/dtos/update-inventory.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { IsOptional, IsNumber, IsString, MaxLength } from "class-validator";
+
+import { CreateInventoryDto } from "./create-inventory.dto";
+
+export class UpdateInventoryDto extends PartialType(CreateInventoryDto) {
+    @ApiProperty({ example: 'Ferreteria', description: 'Financial capital' })
+    @IsOptional()
+    @IsString()
+    @MaxLength(200)
+    company_name: string;
+
+    @ApiProperty({ example: 30000.00, description: 'Financial capital' })
+    @IsOptional()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    capital: number;
+
+    @ApiProperty({ example: 5000.0, description: 'The amount they have to pay you' })
+    @IsOptional()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    service_charge: number;
+}
