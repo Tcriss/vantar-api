757bd6f166df25db6d299d99b3ad7d7488867b6d
Cristian <do.cristian.tr@hotmail.com>
2024-06-20 08:46:51 -0400

    refactor: users module swagger decorators

--- src/users/application/decorators/open-api.decorator.ts
+++ src/users/application/decorators/open-api.decorator.ts
@@ -1,4 +1,4 @@
-import { applyDecorators } from '@nestjs/common';
+import { HttpStatus, applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiQuery, ApiParam } from '@nestjs/swagger';
 
 import { UserEntity } from '../../domain/entities/user.entity';
@@ -7,9 +7,10 @@ export const ApiGetUsers = () => applyDecorators(
     ApiBearerAuth(),
     ApiOperation({ summary: 'Get users' }),
     ApiResponse({ type: UserEntity, isArray: true }),
-    ApiResponse({ status: 403, description: 'Without enough permissions' }),
-    ApiResponse({ status: 500, description: 'Server error' }),
-    ApiQuery({ name: 'page', required: true, example: '0, 10' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Credentials missing' }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Without enough permissions' }),
+    ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Server error' }),
+    ApiQuery({ name: 'page', required: true, example: '0, 10', description: 'pagination settings, first value is skip, and second is take' }),
     ApiQuery({ name: 'q', required: false, description: 'search param to filter results' })
 );
 
@@ -17,36 +18,37 @@ export const ApiGetUser = () => applyDecorators(
     ApiBearerAuth(),
     ApiOperation({ summary: 'Get a user' }),
     ApiResponse({ type: UserEntity }),
-    ApiResponse({ status: 404, description: 'User not found' }),
-    ApiResponse({ status: 500, description: 'Server error' }),
-    ApiParam({ name: 'id', description: 'User id', format: 'uuid' })
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' }),
+    ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Server error' }),
+    ApiParam({ name: 'id', description: 'User id', format: 'uuid', required: true })
 );
 
 export const ApiCreateUser = () => applyDecorators(
     ApiOperation({ summary: 'Create user' }),
-    ApiResponse({ status: 201, description: 'User created succesfully' }),
-    ApiResponse({ status: 400, description: 'Validations error' }),
-    ApiResponse({ status: 409, description: 'User already exist or you are already logged, method not allowed' }),
-    ApiResponse({ status: 500, description: 'Internal server error' })
+    ApiResponse({ status: HttpStatus.CREATED, description: 'User created succesfully' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' }),
+    ApiResponse({ status: HttpStatus.NOT_ACCEPTABLE, description: 'User already exist or you are already logged, method not allowed' }),
+    ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
 );
 
 export const ApiUpdateUser = () => applyDecorators(
     ApiBearerAuth(),
     ApiOperation({ summary: 'Update user' }),
-    ApiResponse({ status: 200, description: 'User created succesfully' }),
-    ApiResponse({ status: 400, description: 'Validations error' }),
-    ApiResponse({ status: 404, description: 'User not found' }),
-    ApiResponse({ status: 406, description: 'Wrong credentials' }),
-    ApiResponse({ status: 500, description: 'Internal server error' }),
-    ApiParam({ name: 'id', description: 'User id', format: 'uuid' })
+    ApiResponse({ status: HttpStatus.OK, description: 'User created succesfully' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Wrong credentials' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' }),
+    ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' }),
+    ApiParam({ name: 'id', description: 'User id', format: 'uuid', required: true })
 );
 
 export const ApiDeleteUser = () => applyDecorators(
     ApiBearerAuth(),
     ApiOperation({ summary: 'Delete user' }),
-    ApiResponse({ status: 200, description: 'User deleted succesfully' }),
-    ApiResponse({ status: 400, description: 'Validations error' }),
-    ApiResponse({ status: 404, description: 'User not found' }),
-    ApiResponse({ status: 500, description: 'Internal server error' }),
-    ApiParam({ name: 'id', description: 'User id', format: 'uuid' })
+    ApiResponse({ status: HttpStatus.OK, description: 'User deleted succesfully' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' }),
+    ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' }),
+    ApiParam({ name: 'id', description: 'User id', format: 'uuid', required: true })
 );
