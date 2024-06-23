d1d6a5f50c5ec3582ec5ece8f9f96d0758c0e8c6
Cristian <do.cristian.tr@hotmail.com>
2024-06-23 13:48:50 -0400

    refactor: swagger decorators for inventories module

--- src/inventories/application/decorators/open-api.decorator.ts
+++ src/inventories/application/decorators/open-api.decorator.ts
@@ -1,41 +1,45 @@
-import { applyDecorators } from "@nestjs/common";
+import { HttpStatus, applyDecorators } from "@nestjs/common";
 import { ApiOperation, ApiQuery, ApiResponse } from "@nestjs/swagger";
 
 import { InventoryEntity } from "../../domain/entities/inventory.entity";
 
 export const ApiGetInventories = () => applyDecorators(
     ApiOperation({ summary: "Get all customer's inventories" }),
-    ApiResponse({ status: 200, type: InventoryEntity, isArray: true }),
-    ApiResponse({ status: 400, description: 'Page param missing' }),
-    ApiQuery({ name: 'page', required: true }),
+    ApiResponse({ status: HttpStatus.OK, type: InventoryEntity, isArray: true }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden resource' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Page param missing' }),
+    ApiQuery({ name: 'page', required: true, description: 'Field that let you paginate the data, take 10 (inventories), skip 1 (skip only one inventory)' }),
     ApiQuery({ name: 'fields', required: false, description: 'Fields you want to fetch in your response' }),
-    ApiQuery({ name: 'q', required: false, description: 'Query earch term' })
+    //ApiQuery({ name: 'q', required: false, description: 'Query earch term' })
 );
 
 export const ApiGetInventory = () => applyDecorators(
     ApiOperation({ summary: "Get a inventory by id" }),
-    ApiResponse({ status: 200, type: InventoryEntity }),
-    ApiResponse({ status: 400, description: 'Invalid id' }),
-    ApiResponse({ status: 404, description: 'Inventory not found' }),
+    ApiResponse({ status: HttpStatus.OK, type: InventoryEntity }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid id' }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden resource' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Inventory not found' }),
     ApiQuery({ name: 'fields', required: false, description: 'Fields you want to fetch in your response' }),
 );
 
 export const ApiCreateInventory = () => applyDecorators(
     ApiOperation({ summary: 'Create a inventory' }),
-    ApiResponse({ status: 201, description: 'Inventory created succesfully' }),
-    ApiResponse({ status: 400, description: 'Validations error' })
+    ApiResponse({ status: HttpStatus.CREATED, description: 'Inventory created succesfully' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' })
 );
 
 export const ApiUpdateInventory = () => applyDecorators(
     ApiOperation({ summary: 'Update inventory' }),
-    ApiResponse({ status: 200, description: 'Inventory updated' }),
-    ApiResponse({ status: 400, description: 'Validations error' }),
-    ApiResponse({ status: 404, description: 'Inventory not found' })
+    ApiResponse({ status: HttpStatus.OK, description: 'Inventory updated' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Validations error' }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden resource' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Inventory not found' })
 );
 
 export const ApiDeleteInventory = () => applyDecorators(
     ApiOperation({ summary: 'Delete inventory' }),
-    ApiResponse({ status: 200, description: 'Inventory deleted' }),
-    ApiResponse({ status: 400, description: 'Invalid id' }),
-    ApiResponse({ status: 404, description: 'Inventory not found' })
+    ApiResponse({ status: HttpStatus.OK, description: 'Inventory deleted' }),
+    ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid id' }),
+    ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden resource' }),
+    ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Inventory not found' })
 );
