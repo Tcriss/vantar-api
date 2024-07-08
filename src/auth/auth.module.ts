755cb61dc5b4ee9775130f9b0ba32258abe36a39
Cristian <do.cristian.tr@hotmail.com>
2024-07-08 11:09:34 -0400

    refactor: jwt module & cors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './application/services/auth.service';
 import { AuthController } from './infrastructure/controllers/auth.controller';
-import { jwtFactory } from './application/config/jwt.factory';
 import { GoogleAuthStrategy } from './application/strategies/google.strategy';
 import { CommonModule } from '../common/common.module';
 import { UserModule } from '../users/user.module';
@@ -16,11 +14,7 @@ import { UserModule } from '../users/user.module';
   ],
   controllers: [AuthController],
   imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: jwtFactory
-    }),
+    JwtModule.register({}),
     CommonModule,
     UserModule
   ]
