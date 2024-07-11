2f182c4602ff90be979d1483125418df04b6eb11
Cristian <do.cristian.tr@hotmail.com>
2024-07-11 10:53:21 -0400

    fix: product list repository issues

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -5,10 +5,10 @@ import { ProductService } from './application/services/product.service';
 import { ProductRepository } from './infrastructure/repositories/product/product.repository';
 import { ProductListRepository } from './infrastructure/repositories/product-list/product-list.repositroy';
 import { ProductController } from './infrastructure/controllers/product.controller';
-import { Repository } from '../common/domain/entities';
 import { ProductEntity } from './domain/entities/product.entity';
 import { DatabaseModule } from '../database/database.module';
 import { ProductListRepositoryToken } from './application/decotators';
+import { Repository } from '../common/domain/entities';
 
 @Module({
     providers: [
