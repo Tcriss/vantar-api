f1f5e78d7baa497d1ce8603ede32eefed05dbc2b
Cristian <do.cristian.tr@hotmail.com>
2024-08-06 16:50:59 -0400

    feat: helmet for securing headers

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
-import { appConfig, swaggerOptions } from './common/application/config';
+import { appConfig, swaggerOptions, helmetConfig } from './common/application/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, appConfig);
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   const configService = app.get<ConfigService>(ConfigService);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: { tagsSorter: 'alpha' }
-  });
-
+  SwaggerModule.setup('api', app, document, { swaggerOptions: { tagsSorter: 'alpha' } });
+  app.use(helmet(helmetConfig));
   await app.listen(configService.get<number>('PORT') || 2020, '0.0.0.0');
   console.log(`Application running on: ${await app.getUrl()}`);
 }
