6bbbca9dbe6eb2115a64c64dd023f0517bb49e6c
Cristian <do.cristian.tr@hotmail.com>
2024-08-13 12:02:38 -0400

    feat: create user guard
    This guard was created to not let customers create accounts when they're logged in, and only allowed admins to create others admin.

---
+++ src/users/application/guards/create-user.guard.spec.ts
@@ -0,0 +1,7 @@
+import { CreateUserGuard } from './create-user.guard';
+
+describe('CreateUserGuard', () => {
+  it('should be defined', () => {
+    expect(new CreateUserGuard()).toBeDefined();
+  });
+});
