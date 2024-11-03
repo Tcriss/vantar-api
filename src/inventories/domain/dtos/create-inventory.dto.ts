a01db115ad2c7826bfe48bdc1c3ee7b67dfe501a
tcriss <do.cristian.tr@hotmail.com>
2024-11-03 16:26:31 -0500

    refactor: update swagger to include shop_id from inventories module

--- src/inventories/domain/dtos/create-inventory.dto.ts
+++ src/inventories/domain/dtos/create-inventory.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsArray, IsNotEmpty, IsNumber, ValidateNested } from "class-validator";
+import { IsArray, IsNotEmpty, IsNumber, IsUUID, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
 
 import { CreateProductListDto } from "@products/domain/dtos";
 
 export class CreateInventoryDto {
+    @ApiProperty({ format: 'uuid' })
+    @IsUUID()
+    @IsNotEmpty()
+    shop_id: string;
+    
     @ApiProperty({ example: 3000.00 })
     @IsNotEmpty()
     @IsNumber({ maxDecimalPlaces: 2 })
